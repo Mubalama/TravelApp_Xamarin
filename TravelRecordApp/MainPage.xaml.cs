4afc6d9e857e16abacb0a4ab85b39757ad26484e
Mubalama Richard <mubalama@live.com>
2019-11-15 23:51:51 +1100

    New added files

---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogingButton_Clicked(object sender, EventArgs e)
+        {
+
+            // Form validation for email and password to check if they are not empty
+
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+            //string message = "Please fill in all the fields !";
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                //lblMessage.Text = message;
+            }
+            else
+            {
+                //lblMessage.Text = "You have successfully logged in"
+                //Navigation to another page
+                Navigation.PushAsync(new HomePage());
+            }
+
+        }
+    }
+}
